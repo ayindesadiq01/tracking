fe3c416af39340a2c7fe6cd911b73b33d02723cb
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-03-12 14:55:07 +0100

    udate

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ const menuList = document.querySelector('.menu-list')
 const hidden = document.querySelector('.hidden')
 
 main.addEventListener('click', function(){
- menuList.classList.remove('hidden');
-})
+ hidden.style.display = 'block';
+ if ( hidden.style.display = 'block') {
+  main.classList.add('hidden')
+ }
+});
+closeTab.addEventListener('click', function(){
+  hidden.style.display = 'none';
+ if ( hidden.style.display = 'none') {
+  main.classList.remove('hidden')
+ }
+});
